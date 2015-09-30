c70cb1477cc46827485d1cb576ded16952bdc419
wangchi0113 <wangchi@wujingsh.com>
2015-09-30 01:18:06 -0400

    Add Insert Functionality

--- both/2_collections.js
+++ both/2_collections.js
@@ -2,6 +2,51 @@ var Collections = {};
 
 Meteor.isClient && Template.registerHelper("Collections", Collections);
 
+Classes = new Mongo.Collection("classes");
+Classes.attachSchema(Schemas.Classes);
+
+Classes.allow({
+  insert: function () { return true; },
+  update: function () { return true; },
+  remove: function () { return true; }
+});
+
+Employees = new Mongo.Collection("employees");
+Employees.attachSchema(Schemas.Employees);
+
+Employees.allow({
+  insert: function () { return true; },
+  update: function () { return true; },
+  remove: function () { return true; }
+});
+
+Factories = new Mongo.Collection("factories");
+Factories.attachSchema(Schemas.Factories);
+
+Factories.allow({
+  insert: function () { return true; },
+  update: function () { return true; },
+  remove: function () { return true; }
+});
+
+Areas = new Mongo.Collection("areas");
+Areas.attachSchema(Schemas.Areas);
+
+Areas.allow({
+  insert: function () { return true; },
+  update: function () { return true; },
+  remove: function () { return true; }
+});
+
+Stores = new Mongo.Collection("stores");
+Stores.attachSchema(Schemas.Stores);
+
+Stores.allow({
+  insert: function () { return true; },
+  update: function () { return true; },
+  remove: function () { return true; }
+});
+
 People = Collections.People = new Mongo.Collection("People");
 People.attachSchema(Schemas.Person);
 
